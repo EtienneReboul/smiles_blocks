8b5c3507bee08a923443effbf516df770636c401
Etienne Reboul <reboul@ibpc.fr>
2026-01-22 11:33:30 +0100

    add file paths configuration to manage project directories

---
+++ smiles_blocks/files.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+__all__ = [
+    "PROJ_ROOT",
+    "DATA_DIR",
+    "RAW_DATA_DIR",
+    "INTERIM_DATA_DIR",
+    "PROCESSED_DATA_DIR",
+    "EXTERNAL_DATA_DIR",
+    "MODELS_DIR",
+    "REPORTS_DIR",
+    "FIGURES_DIR",
+]
+
+# Paths
+PROJ_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJ_ROOT / "data"
+RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
+MODELS_DIR = PROJ_ROOT / "models"
+
+REPORTS_DIR = PROJ_ROOT / "reports"
+FIGURES_DIR = REPORTS_DIR / "figures"
